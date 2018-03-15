b54e7849c85a6d03c98e2a5ab477b2bf05fb0d0b
Andre Miras <andre.miras@gmail.com>
2018-03-15 23:16:10 +0100

    Fixes provider
    Use https://api.myetherapi.com/eth

--- src/pyetheroll.py
+++ src/pyetheroll.py
@@ -3,10 +3,14 @@
 """
 Python Etheroll library.
 """
+from __future__ import print_function
+
 import os
 import json
-from web3 import Web3, HTTPProvider, TestRPCProvider
+from eth_tester import EthereumTester
+from web3 import Web3, HTTPProvider
 from web3.contract import ConciseContract
+from web3.providers.eth_tester import EthereumTesterProvider
 
 
 class Etheroll:
@@ -14,8 +18,12 @@ class Etheroll:
     CONTRACT_ADDRESS = '0xddf0d0b9914d530e0b743808249d9af901f1bd01'
 
     def __init__(self):
-        # self.web3 = Web3(RPCProvider(host='localhost', port='8545'))
-        self.web3 = Web3(TestRPCProvider())
+        ethereum_tester = EthereumTester()
+        # self.provider = EthereumTesterProvider(ethereum_tester)
+        # self.provider = HTTPProvider('https://ropsten.infura.io')
+        self.provider = HTTPProvider('https://api.myetherapi.com/eth')
+        self.web3 = Web3(self.provider)
+        print("blockNumber:", self.web3.eth.blockNumber)
         location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(str(os.path.join(location, 'contract_abi.json')), 'r') as abi_definition:
           self.abi = json.load(abi_definition)
@@ -26,7 +34,7 @@ class Etheroll:
 
 def play_with_contract():
     etheroll = Etheroll()
-    min_bet = etheroll.contract.minBet
+    min_bet = etheroll.contract.minBet()
     print("min_bet:", min_bet)
     pending = etheroll.contract.playerWithdrawPendingTransactions()
     print("pending:", pending)

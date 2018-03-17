7ebc07cc29bbfb8fc9c9ca6cf6a1c7270d2cf619
Andre Miras <andre.miras@gmail.com>
2018-03-17 14:09:58 +0100

    Events definition from ABI

--- src/pyetheroll.py
+++ src/pyetheroll.py
@@ -15,13 +15,17 @@ from web3.providers.eth_tester import EthereumTesterProvider
 
 class Etheroll:
 
-    CONTRACT_ADDRESS = '0xddf0d0b9914d530e0b743808249d9af901f1bd01'
+    # Main network
+    # CONTRACT_ADDRESS = '0xddf0d0b9914d530e0b743808249d9af901f1bd01'
+    # Testnet
+    CONTRACT_ADDRESS = '0xFE8a5f3a7Bb446e1cB4566717691cD3139289ED4'
 
     def __init__(self):
         ethereum_tester = EthereumTester()
         # self.provider = EthereumTesterProvider(ethereum_tester)
-        # self.provider = HTTPProvider('https://ropsten.infura.io')
-        self.provider = HTTPProvider('https://api.myetherapi.com/eth')
+        self.provider = HTTPProvider('https://ropsten.infura.io')
+        # self.provider = HTTPProvider('https://api.myetherapi.com/rop')
+        # self.provider = HTTPProvider('https://api.myetherapi.com/eth')
         self.web3 = Web3(self.provider)
         print("blockNumber:", self.web3.eth.blockNumber)
         location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -34,17 +38,27 @@ class Etheroll:
             abi=self.abi, address=self.CONTRACT_ADDRESS,
             ContractFactoryClass=contract_factory_class)
 
+    def events_abi(self):
+        """
+        Returns only ABI definition of type "event".
+        """
+        return [a for a in self.abi if a['type'] == 'event']
+
     def events_definitions(self):
         """
-        Returns all events definitions.
+        Returns all events definitions (built from ABI definition).
+        e.g.
+        >>> {"LogRefund": "LogRefund(bytes32,address,uint256)"}
         """
-        events_definitions = {
-            "LogBet": "LogBet(bytes32,address,uint256,uint256,uint256,uint256)",
-            "LogResult": "LogResult(uint256,bytes32,address,uint256,uint256,uint256,int256,bytes32,uint256)",
-            "LogRefund": "LogRefund(bytes32,address,uint256)",
-            "LogOwnerTransfer": "LogOwnerTransfer(address,uint256)",
-        }
+        events_definitions = {}
+        events_abi = self.events_abi()
+        for event_abi in events_abi:
+            event_name = event_abi['name']
+            types = ','.join([x['type'] for x in event_abi['inputs']])
+            event_definition = "%s(%s)" % (event_name, types)
+            events_definitions.update({event_name: event_definition})
         return events_definitions
+
 
     def events_signatures(self):
         """
@@ -80,10 +94,13 @@ def play_with_contract():
     etheroll = Etheroll()
     min_bet = etheroll.contract.call().minBet()
     print("min_bet:", min_bet)
-    log_bet_events = etheroll.contract.pastEvents('LogBet').get(only_changes=False)
+    # log_bet_events = etheroll.contract.pastEvents('LogBet').get(only_changes=False)
     # etheroll.web3.eth.filter({'address': Etheroll.CONTRACT_ADDRESS, 'topics': topics})
+    events_definitions = etheroll.events_definitions()
+    print(events_definitions)
     events_signatures = etheroll.events_signatures()
-    events_logs = etheroll.events_logs(['LogBet'])
+    # events_logs = etheroll.events_logs(['LogBet'])
+    print(events_signatures)
     pending = etheroll.contract.call().playerWithdrawPendingTransactions()
     print("pending:", pending)
 

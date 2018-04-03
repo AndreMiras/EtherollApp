13cf21eec2aeec92602ffb4c2358f64c23005c1c
Andre Miras <andre.miras@gmail.com>
2018-04-04 00:43:01 +0200

    greenlet recipe fixes

--- src/python-for-android/recipes/greenlet/__init__.py
+++ src/python-for-android/recipes/greenlet/__init__.py
@@ -13,12 +13,16 @@ class GreenletRecipe(PythonRecipe):
         env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
         # required additional library and path for Crystax
         if self.ctx.ndk == 'crystax':
-            env['LDFLAGS'] += ' -L{}'.format(os.path.join(self.ctx.bootstrap.build_dir, 'libs', arch.arch))
-            env['LDFLAGS'] += ' -lpython{}m'.format(self.ctx.python_recipe.version[0:3])
+            # only keeps major.minor (discards patch)
+            python_version = self.ctx.python_recipe.version[0:3]
+            ndk_dir_python = os.path.join(self.ctx.ndk_dir, 'sources/python/', python_version)
+            env['LDFLAGS'] += ' -L{}'.format(os.path.join(ndk_dir_python, 'libs', arch.arch))
+            env['LDFLAGS'] += ' -lpython{}m'.format(python_version)
             # until `pythonforandroid/archs.py` gets merged upstream:
             # https://github.com/kivy/python-for-android/pull/1250/files#diff-569e13021e33ced8b54385f55b49cbe6
-            env['CFLAGS'] += ' -I{}/sources/python/{}/include/python/'.format(
-                self.ctx.ndk_dir, self.ctx.python_recipe.version[0:3])
+            env['CFLAGS'] += ' -I{}/include/python/'.format(ndk_dir_python)
+            print("self.ctx.ndk_dir:", self.ctx.ndk_dir)
+            print("ndk_dir_python:", ndk_dir_python)
         return env
 
 

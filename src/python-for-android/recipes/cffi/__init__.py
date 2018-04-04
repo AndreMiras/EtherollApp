b125b724e9f2dd528f3b7fe4992296cd920b5e5c
Andre Miras <andre.miras@gmail.com>
2018-04-04 23:04:20 +0200

    More recipe fixes

--- src/python-for-android/recipes/cffi/__init__.py
+++ src/python-for-android/recipes/cffi/__init__.py
@@ -33,6 +33,15 @@ class CffiRecipe(CompiledComponentsPythonRecipe):
             self.ctx.get_site_packages_dir(),
             env['BUILDLIB_PATH'],
         ])
+        if self.ctx.ndk == 'crystax':
+            # only keeps major.minor (discards patch)
+            python_version = self.ctx.python_recipe.version[0:3]
+            ndk_dir_python = os.path.join(self.ctx.ndk_dir, 'sources/python/', python_version)
+            env['LDFLAGS'] += ' -L{}'.format(os.path.join(ndk_dir_python, 'libs', arch.arch))
+            env['LDFLAGS'] += ' -lpython{}m'.format(python_version)
+            # until `pythonforandroid/archs.py` gets merged upstream:
+            # https://github.com/kivy/python-for-android/pull/1250/files#diff-569e13021e33ced8b54385f55b49cbe6
+            env['CFLAGS'] += ' -I{}/include/python/'.format(ndk_dir_python)
         return env
 
 

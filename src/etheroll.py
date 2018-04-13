1ac5a7568fb8b2871ecbdad22cc524dec48569fe
Andre Miras <andre.miras@gmail.com>
2018-04-13 19:15:30 +0200

    Account creation screen and tests, fixes #13

--- src/etheroll.py
+++ src/etheroll.py
@@ -17,6 +17,7 @@ from raven import Client
 from raven.conf import setup_logging
 from raven.handlers.logging import SentryHandler
 
+from ethereum_utils import AccountUtils
 from utils import Dialog, patch_find_library_android, patch_typing_python351
 from version import __version__
 
@@ -70,7 +71,7 @@ class SwitchAccount(BoxLayout):
         self.controller = App.get_running_app().root
         account_list_id = self.ids.account_list_id
         account_list_id.clear_widgets()
-        accounts = self.controller.pyethapp.services.accounts
+        accounts = self.controller.account_utils.get_account_list()
         if len(accounts) == 0:
             self.on_empty_account_list()
         for account in accounts:
@@ -80,11 +81,121 @@ class SwitchAccount(BoxLayout):
     @staticmethod
     def on_empty_account_list():
         controller = App.get_running_app().root
-        keystore_dir = controller.pyethapp.services.accounts.keystore_dir
+        keystore_dir = controller.account_utils.keystore_dir
         title = "No account found"
         body = "No account found in:\n%s" % keystore_dir
         dialog = Dialog.create_dialog(title, body)
         dialog.open()
+
+
+class CreateNewAccount(BoxLayout):
+    """
+    Makes it possible to create json keyfiles.
+    """
+
+    new_password1 = StringProperty()
+    new_password2 = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(CreateNewAccount, self).__init__(**kwargs)
+        Clock.schedule_once(lambda dt: self.setup())
+
+    def setup(self):
+        """
+        Sets security vs speed default values.
+        Plus hides the advanced widgets.
+        """
+        self.controller = App.get_running_app().root
+
+    def verify_password_field(self):
+        """
+        Makes sure passwords are matching and are not void.
+        """
+        passwords_matching = self.new_password1 == self.new_password2
+        passwords_not_void = self.new_password1 != ''
+        return passwords_matching and passwords_not_void
+
+    def verify_fields(self):
+        """
+        Verifies password fields are valid.
+        """
+        return self.verify_password_field()
+
+    @staticmethod
+    def try_unlock(account, password):
+        """
+        Just as a security measure, verifies we can unlock
+        the newly created account with provided password.
+        """
+        # making sure it's locked first
+        account.lock()
+        try:
+            account.unlock(password)
+        except ValueError:
+            title = "Unlock error"
+            body = ""
+            body += "Couldn't unlock your account.\n"
+            body += "The issue should be reported."
+            dialog = Dialog.create_dialog(title, body)
+            dialog.open()
+            return
+
+    # @mainthread
+    def on_account_created(self, account):
+        """
+        Switches to the newly created account.
+        Clears the form.
+        """
+        self.controller.switch_account_screen.current_account = account
+        self.new_password1 = ''
+        self.new_password2 = ''
+
+    # @mainthread
+    def toggle_widgets(self, enabled):
+        """
+        Enables/disables account creation widgets.
+        """
+        self.disabled = not enabled
+
+    def show_redirect_dialog(self):
+        title = "Account created, redirecting..."
+        body = ""
+        body += "Your account was created, "
+        body += "you will be redirected to the overview."
+        dialog = Dialog.create_dialog(title, body)
+        dialog.open()
+
+    def load_landing_page(self):
+        """
+        Returns to the landing page.
+        """
+        screen_manager = self.controller.screen_manager
+        screen_manager.transition.direction = 'right'
+        screen_manager.current = 'roll_screen'
+
+    # @run_in_thread
+    def create_account(self):
+        """
+        Creates an account from provided form.
+        Verify we can unlock it.
+        Disables widgets during the process, so the user doesn't try
+        to create another account during the process.
+        """
+        self.toggle_widgets(False)
+        if not self.verify_fields():
+            Dialog.show_invalid_form_dialog()
+            self.toggle_widgets(True)
+            return
+        password = self.new_password1
+        Dialog.snackbar_message("Creating account...")
+        account = self.controller.account_utils.new_account(password=password)
+        Dialog.snackbar_message("Created!")
+        self.toggle_widgets(True)
+        self.on_account_created(account)
+        CreateNewAccount.try_unlock(account, password)
+        self.show_redirect_dialog()
+        self.load_landing_page()
+        return account
 
 
 class CustomToolbar(Toolbar):
@@ -334,12 +445,7 @@ class Controller(FloatLayout):
     def _init_pyethapp(self, keystore_dir=None):
         if keystore_dir is None:
             keystore_dir = self.get_keystore_path()
-        # must be imported after `patch_find_library_android()`
-        from devp2p.app import BaseApp
-        from pyethapp.accounts import AccountsService
-        self.pyethapp = BaseApp(
-            config=dict(accounts=dict(keystore_dir=keystore_dir)))
-        AccountsService.register_with_app(self.pyethapp)
+        self.account_utils = AccountUtils(keystore_dir=keystore_dir)
 
     @property
     def pyetheroll(self):
@@ -439,6 +545,10 @@ class Controller(FloatLayout):
     @property
     def navigation(self):
         return self.ids.navigation_id
+
+    @property
+    def screen_manager(self):
+        return self.ids.screen_manager_id
 
     @property
     def roll_screen(self):

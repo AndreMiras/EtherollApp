ce975bf5dc816cb26e4d59792434b72fbf590c8c
Andre Miras <andre.miras@gmail.com>
2018-04-28 00:17:06 +0200

    Dedicated switch account files, refs #43

--- src/etheroll/controller.py
+++ src/etheroll/controller.py
@@ -8,13 +8,12 @@ from kivy.core.clipboard import Clipboard
 from kivy.garden.qrcode import QRCodeWidget
 from kivy.logger import LOG_LEVELS, Logger
 from kivy.metrics import dp
-from kivy.properties import NumericProperty, ObjectProperty, StringProperty
+from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen
 from kivy.utils import platform
 from kivymd.bottomsheet import MDListBottomSheet
-from kivymd.list import OneLineListItem
 from kivymd.theming import ThemeManager
 from kivymd.toolbar import Toolbar
 from raven import Client
@@ -39,60 +38,6 @@ load_kv_from_py(__file__)
 
 class PasswordForm(BoxLayout):
     password = StringProperty()
-
-
-class SwitchAccount(BoxLayout):
-
-    def __init__(self, **kwargs):
-        super(SwitchAccount, self).__init__(**kwargs)
-        self.register_event_type('on_account_selected')
-
-    def on_release(self, list_item):
-        """
-        Fires on_account_selected() event.
-        """
-        self.dispatch('on_account_selected', list_item.account)
-
-    def on_account_selected(self, *args):
-        """
-        Default handler.
-        """
-        pass
-
-    def create_item(self, account):
-        """
-        Creates an account list item from given account.
-        """
-        address = "0x" + account.address.hex()
-        list_item = OneLineListItem(text=address)
-        # makes sure the address doesn't overlap on small screen
-        list_item.ids._lbl_primary.shorten = True
-        list_item.account = account
-        list_item.bind(on_release=lambda x: self.on_release(x))
-        return list_item
-
-    def load_account_list(self):
-        """
-        Fills account list widget from library account list.
-        """
-        self.controller = App.get_running_app().root
-        account_list_id = self.ids.account_list_id
-        account_list_id.clear_widgets()
-        accounts = self.controller.account_utils.get_account_list()
-        if len(accounts) == 0:
-            self.on_empty_account_list()
-        for account in accounts:
-            list_item = self.create_item(account)
-            account_list_id.add_widget(list_item)
-
-    @staticmethod
-    def on_empty_account_list():
-        controller = App.get_running_app().root
-        keystore_dir = controller.account_utils.keystore_dir
-        title = "No account found"
-        body = "No account found in:\n%s" % keystore_dir
-        dialog = Dialog.create_dialog(title, body)
-        dialog.open()
 
 
 class CreateNewAccount(BoxLayout):
@@ -240,30 +185,6 @@ class ImportKeystore(BoxLayout):
         """
         controller = App.get_running_app().root
         self.keystore_path = controller.get_keystore_path()
-
-
-class SwitchAccountScreen(SubScreen):
-    current_account = ObjectProperty(allownone=True)
-
-    def __init__(self, **kwargs):
-        super(SwitchAccountScreen, self).__init__(**kwargs)
-        Clock.schedule_once(self._after_init)
-
-    def _after_init(self, dt):
-        """
-        Binds SwitchAccount.on_account_selected() event.
-        """
-        switch_account = self.ids.switch_account_id
-        switch_account.bind(
-            on_account_selected=lambda
-            instance, account: self.on_account_selected(account))
-
-    def on_account_selected(self, account):
-        """
-        Sets current account and loads previous screen.
-        """
-        self.current_account = account
-        self.on_back()
 
 
 class SettingsScreen(SubScreen):

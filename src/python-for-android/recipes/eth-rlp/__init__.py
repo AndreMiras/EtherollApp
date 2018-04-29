f9b17382d86ab76d53af93a8789bdb397de9fde6
Andre Miras <andre.miras@gmail.com>
2018-04-30 00:02:01 +0200

    Update eth-rlp recipe

--- src/python-for-android/recipes/eth-rlp/__init__.py
+++ src/python-for-android/recipes/eth-rlp/__init__.py
@@ -2,8 +2,8 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class EthRlpRecipe(PythonRecipe):
-    version = '0.1.0'
-    url = 'https://github.com/ethereum/eth-rlp/archive/vv{version}.tar.gz'
+    version = '0.1.2'
+    url = 'https://github.com/ethereum/eth-rlp/archive/v{version}.tar.gz'
     depends = [('python2', 'python3crystax'), 'setuptools']
     patches = ['disable-setuptools-markdown.patch']
 

5f246356e77081ec51118f7b48dc405809888760
Andre Miras <andre.miras@gmail.com>
2018-05-05 18:24:16 +0200

    Adds changelog to about screen, fixes #73

---
+++ src/CHANGELOG.md
@@ -0,0 +1,52 @@
+# Change Log
+
+## [Unreleased]
+
+  - File split & refactoring, refs #43
+  - Lazy screen loading, refs #47
+  - Pull to refresh roll history, refs #55
+  - UI testing, refs #61, #64
+  - Ubuntu Bionic 18.04 support, refs #70, #71
+
+
+## [v20180418]
+
+  - Per account bet history, refs #16
+  - Remove topic3 workaround, refs #48
+  - Handle unresolved bets, refs #51
+
+
+## [v20180414]
+
+  - UI testing, refs #10
+  - Account creation, refs #13
+  - Setup continuous integration, refs #18
+  - Dockerized project, refs #19
+  - Download ABIs dynamically and cache it, refs #41
+  - Show account QR Code, refs #44
+  - Quick account actions, refs #45
+  - Chances input binding fixes, refs #46
+
+
+## [v20180405]
+
+  - Switch testnet/mainnet from UI, refs #11, #21
+  - Integrate with Sentry, refs #17
+  - Android splash screen and icon, refs #24
+
+
+## [v20180404]
+
+  - Simple account managment refs, #6, #7, #14
+  - Porting to Android, refs #12
+  - Python3 + greenlet install issue, refs #25
+  - Gevent compilation issues, refs #29
+  - FileNotFoundError lib crypto on Android, refs #30
+  - PyYAML ImportError: No module named 'error', refs #31
+  - Android OpenSSL support, refs #32
+
+
+## [v20180327]
+
+  - Place bet on Testnet, refs #1
+  - Simple demo UI, refs #5
--- src/etheroll/about.py
+++ src/etheroll/about.py
@@ -1,5 +1,8 @@
+import os
+
 from kivy.clock import Clock
 from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
 
 from etheroll.utils import SubScreen, load_kv_from_py
 from version import __version__
@@ -7,13 +10,13 @@ from version import __version__
 load_kv_from_py(__file__)
 
 
-class AboutScreen(SubScreen):
+class AboutOverview(BoxLayout):
     project_page_property = StringProperty(
         "https://github.com/AndreMiras/EtherollApp")
     about_text_property = StringProperty()
 
     def __init__(self, **kwargs):
-        super(AboutScreen, self).__init__(**kwargs)
+        super(AboutOverview, self).__init__(**kwargs)
         Clock.schedule_once(lambda dt: self.load_about())
 
     def load_about(self):
@@ -23,3 +26,25 @@ class AboutScreen(SubScreen):
             "[color=00BFFF][ref=github]" + \
             self.project_page_property + \
             "[/ref][/color]"
+
+
+class AboutChangelog(BoxLayout):
+    changelog_text_property = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(AboutChangelog, self).__init__(**kwargs)
+        Clock.schedule_once(lambda dt: self.load_changelog())
+
+    @staticmethod
+    def src_dir():
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+
+    def load_changelog(self):
+        changelog_path = os.path.join(self.src_dir(), 'CHANGELOG.md')
+        with open(changelog_path, 'r') as f:
+            self.changelog_text_property = f.read()
+        f.close()
+
+
+class AboutScreen(SubScreen):
+    pass

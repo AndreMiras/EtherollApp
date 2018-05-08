AndreMiras/EtherollApp

commit 5098d1a7b06d0003ca6bdbf9452f9dd183bc275e

ETH to wei conversion issues on Android, fixes #77
For some reason `w3.toWei(value, 'ether')` is misbehaving on Android.
Now using a more simple for of 1e18.

src/pyetheroll.py
@@ -360,7 +360,10 @@ class Etheroll:
         Returns transaction hash.
         """
         roll_under = chances
-        value_wei = w3.toWei(bet_size_ether, 'ether')
+        # `w3.toWei` one has some issues on Android, see:
+        # https://github.com/AndreMiras/EtherollApp/issues/77
+        # value_wei = w3.toWei(bet_size_ether, 'ether')
+        value_wei = bet_size_ether * 1e18
         gas = 310000
         gas_price = w3.toWei(4, 'gwei')
         # since Account.load is hanging while decrypting the password

src/tests/test_pyetheroll.py
@@ -528,7 +528,7 @@ class TestEtheroll(unittest.TestCase):
             'gas': 310000,
             'value': 100000000000000000, 'gasPrice': 4000000000
         }
-        expected_call = ((expected_transaction, account.privkey),)
+        expected_call = mock.call(expected_transaction, account.privkey)
         # the method should have been called only once
         expected_calls = [expected_call]
         self.assertEqual(m_signTransaction.call_args_list, expected_calls)

commit 5b41d8c113fd5d355cdf55d1a80ca8c565f11102

value_wei must be an int
Fixes bug introduced in 5098d1a7b06d0003ca6bdbf9452f9dd183bc275e

src/pyetheroll.py
@@ -363,7 +363,7 @@ class Etheroll:
         # `w3.toWei` one has some issues on Android, see:
         # https://github.com/AndreMiras/EtherollApp/issues/77
         # value_wei = w3.toWei(bet_size_ether, 'ether')
-        value_wei = bet_size_ether * 1e18
+        value_wei = int(bet_size_ether * 1e18)
         gas = 310000
         gas_price = w3.toWei(4, 'gwei')
         # since Account.load is hanging while decrypting the password

src/tests/test_pyetheroll.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from tempfile import mkdtemp
 from unittest import mock
 
+from eth_account.internal.transactions import assert_valid_fields
 from ethereum.tools.keys import PBKDF2_CONSTANTS
 from hexbytes.main import HexBytes
 from pyethapp.accounts import Account
@@ -532,6 +533,18 @@ class TestEtheroll(unittest.TestCase):
         # the method should have been called only once
         expected_calls = [expected_call]
         self.assertEqual(m_signTransaction.call_args_list, expected_calls)
+        # also make sure the transaction dict is passing the validation
+        # e.g. scientific notation 1e+17 is not accepted
+        transaction_dict = m_signTransaction.call_args[0][0]
+        assert_valid_fields(transaction_dict)
+        # even though below values are equal
+        self.assertTrue(transaction_dict['value'] == 0.1 * 1e18 == 1e17)
+        # this is not accepted by `assert_valid_fields()`
+        transaction_dict['value'] = 0.1 * 1e18
+        with self.assertRaises(TypeError):
+            assert_valid_fields(transaction_dict)
+        # because float are not accepted
+        self.assertEqual(type(transaction_dict['value']), float)
 
     def test_get_last_bets_transactions(self):
         """
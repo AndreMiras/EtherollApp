255843f386447431ae3f15980d4cf828b273f559
Andre Miras <andre.miras@gmail.com>
2018-10-26 22:48:52 +0200

    Separates utils concerns

--- src/etheroll/ui_utils.py
+++ src/etheroll/ui_utils.py
@@ -1,49 +1,16 @@
 import os
 import threading
-from io import StringIO
 
 from kivy.app import App
 from kivy.clock import mainthread
 from kivy.lang import Builder
-from kivy.logger import LOG_LEVELS
 from kivy.metrics import dp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
-from kivy.utils import platform
 from kivymd.dialog import MDDialog
 from kivymd.label import MDLabel
 from kivymd.snackbar import Snackbar
 from layoutmargin import AddMargin, MarginLayout
-from raven import Client
-from raven.conf import setup_logging
-from raven.handlers.logging import SentryHandler
-
-from version import __version__
-
-
-def run_in_thread(fn):
-    """
-    Decorator to run a function in a thread.
-    >>> 1 + 1
-    2
-    >>> @run_in_thread
-    ... def threaded_sleep(seconds):
-    ...     from time import sleep
-    ...     sleep(seconds)
-    >>> thread = threaded_sleep(0.1)
-    >>> type(thread)
-    <class 'threading.Thread'>
-    >>> thread.is_alive()
-    True
-    >>> thread.join()
-    >>> thread.is_alive()
-    False
-    """
-    def run(*k, **kw):
-        t = threading.Thread(target=fn, args=k, kwargs=kw)
-        t.start()
-        return t
-    return run
 
 
 def load_kv_from_py(f):
@@ -57,76 +24,6 @@ def load_kv_from_py(f):
             filename + '.kv'
         )
     )
-
-
-def configure_sentry(in_debug=False):
-    """
-    Configure the Raven client, or create a dummy one if `in_debug` is `True`.
-    """
-    key = 'b290ecc8934f4cb599e6fa6af6cc5cc2'
-    # the public DSN URL is not available on the Python client
-    # so we're exposing the secret and will be revoking it on abuse
-    # https://github.com/getsentry/raven-python/issues/569
-    secret = '0ae02bcb5a75467d9b4431042bb98cb9'
-    project_id = '1111738'
-    dsn = 'https://{key}:{secret}@sentry.io/{project_id}'.format(
-        key=key, secret=secret, project_id=project_id)
-    if in_debug:
-        client = DebugRavenClient()
-    else:
-        client = Client(dsn=dsn, release=__version__)
-        # adds context for Android devices
-        if platform == 'android':
-            from jnius import autoclass
-            Build = autoclass("android.os.Build")
-            VERSION = autoclass('android.os.Build$VERSION')
-            android_os_build = {
-                'model': Build.MODEL,
-                'brand': Build.BRAND,
-                'device': Build.DEVICE,
-                'manufacturer': Build.MANUFACTURER,
-                'version_release': VERSION.RELEASE,
-            }
-            client.user_context({'android_os_build': android_os_build})
-        # Logger.error() to Sentry
-        # https://docs.sentry.io/clients/python/integrations/logging/
-        handler = SentryHandler(client)
-        handler.setLevel(LOG_LEVELS.get('error'))
-        setup_logging(handler)
-    return client
-
-
-class DebugRavenClient(object):
-    """
-    The DebugRavenClient should be used in debug mode, it just raises
-    the exception rather than capturing it.
-    """
-
-    def captureException(self):
-        raise
-
-
-class StringIOCBWrite(StringIO):
-    """
-    Inherits StringIO, provides callback on write.
-    """
-
-    def __init__(self, initial_value='', newline='\n', callback_write=None):
-        """
-        Overloads the StringIO.__init__() makes it possible to hook a callback
-        for write operations.
-        """
-        self.callback_write = callback_write
-        super(StringIOCBWrite, self).__init__(initial_value, newline)
-
-    def write(self, s):
-        """
-        Calls the StringIO.write() method then the callback_write with
-        given string parameter.
-        """
-        super(StringIOCBWrite, self).write(s)
-        if self.callback_write is not None:
-            self.callback_write(s)
 
 
 class Dialog(object):
